248cc18160d4100d475eebb77ae5009f803afaeb
eazy-man <eazyrev15@gmail.com>
2025-11-05 23:53:51 -0500

    Create hellothere.lua

---
+++ theactualscript/hellothere.lua
@@ -0,0 +1,431 @@
+local ScreenGui1 = Instance.new('ScreenGui')
+ScreenGui1.Parent = game:GetService('CoreGui')
+
+local ImageLabel1 = Instance.new('ImageLabel')
+ImageLabel1.Size = UDim2.new(0, 600, 0, 400)
+ImageLabel1.Position = UDim2.new(0.305194795, 0, 0.326989621, 0)
+ImageLabel1.Draggable = true
+ImageLabel1.BorderSizePixel = 0
+ImageLabel1.BorderColor3 = Color3.new(0, 0, 0)
+ImageLabel1.BackgroundColor3 = Color3.new(1, 1, 1)
+ImageLabel1.Active = true
+ImageLabel1.Image = "rbxassetid://30640839"
+ImageLabel1.Parent = ScreenGui1
+
+local Frame1 = Instance.new('Frame')
+Frame1.Size = UDim2.new(0, 600, 0, 50)
+Frame1.BorderSizePixel = 0
+Frame1.BorderColor3 = Color3.new(0, 0, 0)
+Frame1.BackgroundColor3 = Color3.new(0.152941, 0.152941, 0.152941)
+Frame1.Parent = ImageLabel1
+
+local TextLabel1 = Instance.new('TextLabel')
+TextLabel1.Size = UDim2.new(0, 200, 0, 50)
+TextLabel1.Position = UDim2.new(-0.00147186278, 0, -0.0130102541, 0)
+TextLabel1.BorderSizePixel = 0
+TextLabel1.BorderColor3 = Color3.new(0, 0, 0)
+TextLabel1.BackgroundTransparency = 1
+TextLabel1.BackgroundColor3 = Color3.new(1, 1, 1)
+TextLabel1.TextScaled = true
+TextLabel1.TextWrapped = true
+TextLabel1.TextColor3 = Color3.new(1, 1, 1)
+TextLabel1.Text = "chocosploit raknet fe bypass"
+TextLabel1.FontSize = Enum.FontSize.Size14
+TextLabel1.Font = Enum.Font.SourceSansBold
+TextLabel1.Parent = Frame1
+
+local TextButton1 = Instance.new('TextButton')
+TextButton1.Name = "debrad"
+TextButton1.Size = UDim2.new(0, 200, 0, 50)
+TextButton1.Position = UDim2.new(0.666666687, 0, 0, 0)
+TextButton1.BorderSizePixel = 0
+TextButton1.BorderColor3 = Color3.new(0, 0, 0)
+TextButton1.BackgroundColor3 = Color3.new(1, 1, 1)
+TextButton1.TextColor3 = Color3.new(0, 0, 0)
+TextButton1.Text = "debradify"
+TextButton1.FontSize = Enum.FontSize.Size14
+TextButton1.Font = Enum.Font.SourceSans
+TextButton1.Parent = Frame1
+
+local LocalScript1 = Instance.new('LocalScript')
+LocalScript1.Parent = TextButton1
+
+local ScrollingFrame1 = Instance.new('ScrollingFrame')
+ScrollingFrame1.Size = UDim2.new(0, 601, 0, 350)
+ScrollingFrame1.Position = UDim2.new(-0.00166666671, 0, 0.125, 0)
+ScrollingFrame1.BorderSizePixel = 0
+ScrollingFrame1.BorderColor3 = Color3.new(0, 0, 0)
+ScrollingFrame1.BackgroundTransparency = 1
+ScrollingFrame1.BackgroundColor3 = Color3.new(1, 1, 1)
+ScrollingFrame1.Active = true
+ScrollingFrame1.ScrollBarThickness = 0
+ScrollingFrame1.Parent = ImageLabel1
+
+local TextButton2 = Instance.new('TextButton')
+TextButton2.Name = "harkkek5"
+TextButton2.Size = UDim2.new(0, 100, 0, 50)
+TextButton2.Position = UDim2.new(0.0500000007, 0, 0.0500000007, 0)
+TextButton2.BorderSizePixel = 0
+TextButton2.BorderColor3 = Color3.new(0, 0, 0)
+TextButton2.BackgroundColor3 = Color3.new(1, 1, 1)
+TextButton2.TextScaled = true
+TextButton2.TextWrapped = true
+TextButton2.TextColor3 = Color3.new(0, 0, 0)
+TextButton2.Text = "HARKK3K 5.0 (RARE)"
+TextButton2.FontSize = Enum.FontSize.Size14
+TextButton2.Font = Enum.Font.SourceSans
+TextButton2.Parent = ScrollingFrame1
+
+local LocalScript2 = Instance.new('LocalScript')
+LocalScript2.Parent = TextButton2
+
+local TextButton3 = Instance.new('TextButton')
+TextButton3.Name = "harknade"
+TextButton3.Size = UDim2.new(0, 100, 0, 50)
+TextButton3.Position = UDim2.new(0.289999992, 0, 0.0500000007, 0)
+TextButton3.BorderSizePixel = 0
+TextButton3.BorderColor3 = Color3.new(0, 0, 0)
+TextButton3.BackgroundColor3 = Color3.new(1, 1, 1)
+TextButton3.TextScaled = true
+TextButton3.TextWrapped = true
+TextButton3.TextColor3 = Color3.new(0, 0, 0)
+TextButton3.Text = "HARKNADE (RARE)"
+TextButton3.FontSize = Enum.FontSize.Size14
+TextButton3.Font = Enum.Font.SourceSans
+TextButton3.Parent = ScrollingFrame1
+
+local LocalScript3 = Instance.new('LocalScript')
+LocalScript3.Parent = TextButton3
+
+local TextButton4 = Instance.new('TextButton')
+TextButton4.Name = "doge"
+TextButton4.Size = UDim2.new(0, 100, 0, 50)
+TextButton4.Position = UDim2.new(0.540000021, 0, 0.0500000007, 0)
+TextButton4.BorderSizePixel = 0
+TextButton4.BorderColor3 = Color3.new(0, 0, 0)
+TextButton4.BackgroundColor3 = Color3.new(1, 1, 1)
+TextButton4.TextScaled = true
+TextButton4.TextWrapped = true
+TextButton4.TextColor3 = Color3.new(0, 0, 0)
+TextButton4.Text = "DOGE THAT GOES HARK HARK (RARE)"
+TextButton4.FontSize = Enum.FontSize.Size14
+TextButton4.Font = Enum.Font.SourceSans
+TextButton4.Parent = ScrollingFrame1
+
+local LocalScript4 = Instance.new('LocalScript')
+LocalScript4.Parent = TextButton4
+
+local TextButton5 = Instance.new('TextButton')
+TextButton5.Name = "harktistic"
+TextButton5.Size = UDim2.new(0, 100, 0, 50)
+TextButton5.Position = UDim2.new(0.790000021, 0, 0.0500000007, 0)
+TextButton5.BorderSizePixel = 0
+TextButton5.BorderColor3 = Color3.new(0, 0, 0)
+TextButton5.BackgroundColor3 = Color3.new(1, 1, 1)
+TextButton5.TextScaled = true
+TextButton5.TextWrapped = true
+TextButton5.TextColor3 = Color3.new(0, 0, 0)
+TextButton5.Text = "HARKTISTIC TELEKENISIS (RARE)"
+TextButton5.FontSize = Enum.FontSize.Size14
+TextButton5.Font = Enum.Font.SourceSans
+TextButton5.Parent = ScrollingFrame1
+
+local LocalScript5 = Instance.new('LocalScript')
+LocalScript5.Parent = TextButton5
+
+local TextButton6 = Instance.new('TextButton')
+TextButton6.Name = "harkkek4"
+TextButton6.Size = UDim2.new(0, 100, 0, 50)
+TextButton6.Position = UDim2.new(0.0500000007, 0, 0.150000006, 0)
+TextButton6.BorderSizePixel = 0
+TextButton6.BorderColor3 = Color3.new(0, 0, 0)
+TextButton6.BackgroundColor3 = Color3.new(1, 1, 1)
+TextButton6.TextScaled = true
+TextButton6.TextWrapped = true
+TextButton6.TextColor3 = Color3.new(0, 0, 0)
+TextButton6.Text = "HARKK3K 4.0 (RARE)"
+TextButton6.FontSize = Enum.FontSize.Size14
+TextButton6.Font = Enum.Font.SourceSans
+TextButton6.Parent = ScrollingFrame1
+
+local LocalScript6 = Instance.new('LocalScript')
+LocalScript6.Parent = TextButton6
+
+local TextButton7 = Instance.new('TextButton')
+TextButton7.Name = "harkknifev4"
+TextButton7.Size = UDim2.new(0, 100, 0, 50)
+TextButton7.Position = UDim2.new(0.790000021, 0, 0.25, 0)
+TextButton7.BorderSizePixel = 0
+TextButton7.BorderColor3 = Color3.new(0, 0, 0)
+TextButton7.BackgroundColor3 = Color3.new(1, 1, 1)
+TextButton7.TextScaled = true
+TextButton7.TextWrapped = true
+TextButton7.TextColor3 = Color3.new(0, 0, 0)
+TextButton7.Text = "HARKKNIFEV4 (ULTRA RARE OMG)"
+TextButton7.FontSize = Enum.FontSize.Size14
+TextButton7.Font = Enum.Font.SourceSans
+TextButton7.Parent = ScrollingFrame1
+
+local LocalScript7 = Instance.new('LocalScript')
+LocalScript7.Parent = TextButton7
+
+local LocalScript8 = Instance.new('LocalScript')
+LocalScript8.Parent = ImageLabel1
+
+-- Script @ StarterGui.ScreenGui.ImageLabel.Frame.debrad.LocalScript
+coroutine.wrap(function()
+local script = LocalScript1
+scrolling = script.Parent.Parent.Parent.ScrollingFrame
+script.Parent.MouseButton1Click:Connect(function()
+	scrolling.harkkek5.Text = "T0PK3K 5.0 (FD)"
+	scrolling.harkkek4.Text = "T0PK3K 4.0 (FD)"
+	scrolling.harknade.Text = "Grenade (FD)"
+	scrolling.doge.Text = "Doge Army (FD)"
+	scrolling.harktistic.Text = "Autistic Telekenisis (FD)"
+end)
+end)()
+
+-- Script @ StarterGui.ScreenGui.ImageLabel.ScrollingFrame.harkkek5.LocalScript
+coroutine.wrap(function()
+local script = LocalScript2
+script.Parent.MouseButton1Click:Connect(function()
+	loadstring(game:HttpGet("https://cdn.jsdelivr.net/gh/eazy-man/febypass@refs/heads/main/harkkek5.lua", true))()
+end)
+
+end)()
+
+-- Script @ StarterGui.ScreenGui.ImageLabel.ScrollingFrame.harknade.LocalScript
+coroutine.wrap(function()
+local script = LocalScript3
+script.Parent.MouseButton1Click:Connect(function()
+	-- Config --
+	local minimumsize = Vector3.new(0.7,0.7,0.7) -- Minimum size for a part to get divided, higher numbers = less detailed and bigger/less bricks
+	local surface_between_splitted_parts = 'SmoothNoOutlines' -- the surfacetype between splitted parts
+	local fragmentable = workspace -- all fragmentable objects should be stored in here. dont change unless you only want certain parts to fragment
+	local fillup = 1000 -- it constantly generates new parts until it reaches this number (hacky way to prevent lagspikes if there is a large explosion),change it to 0 if you don´t want it to generate (useless) parts.
+	local maximumstorage = 2000 -- it will recycle parts if the number of parts in the storage doesnt exceed this number
+	local storage_position = Vector3.new(0,0,5000000) -- place them somewhere off the map
+	local stored_partsize = Vector3.new(1,1,1) -- make them small
+	local parts_created_per_frame = 5 -- number of parts being created per frame to fill up the storage
+	local fuse = 2.5 -- grenade fuse
+
+	-- Main --
+	local lp = game:GetService("Players").LocalPlayer
+	local ch = lp.Character
+	local list = {}
+	local brickcount = 0
+	local storage = {}
+
+	function fragmentate(cframe,size,color,explosion_position,explosion_blastradius,backsurface,bottomsurface,frontsurface,leftsurface,rightsurface,topsurface,transparency,reflectance,material)
+		local xi = size.X >= minimumsize.X*(1+explosion_blastradius/16) and 2 or 1 -- to reduce the lagg in large explosions we increase minimumsize based on the explosionradius...
+		local yi = size.Y >= minimumsize.Y*(1+explosion_blastradius/16) and 2 or 1
+		local zi = size.Z >= minimumsize.Z*(1+explosion_blastradius/16) and 2 or 1
+		if not material then material = "Plastic" end
+		if xi == 1 and yi == 1 and zi == 1 or (cframe.p-explosion_position).magnitude > size.magnitude/2 + explosion_blastradius then -- don't fragmentate parts, that are too small to fragmentate or too far away from the explosion
+			if xi == 1 and yi == 1 and zi == 1 then return end -- optional
+			if #storage > 0 then
+				local p = storage[1]
+				p.BrickColor = color
+				p.Size = size
+				p.BackSurface = backsurface
+				p.BottomSurface = bottomsurface
+				p.FrontSurface = frontsurface
+				p.LeftSurface = leftsurface
+				p.RightSurface = rightsurface
+				p.TopSurface = topsurface
+				p.Transparency = transparency
+				p.Material = material
+				p.CFrame = cframe
+				p.Reflectance = reflectance
+				table.remove(storage,1)
+			else
+				local p = Instance.new("Part",fragmentable)
+				p.BrickColor = color
+				p.FormFactor = "Custom"
+				p.Size = size
+				p.BackSurface = backsurface
+				p.BottomSurface = bottomsurface
+				p.FrontSurface = frontsurface
+				p.LeftSurface = leftsurface
+				p.RightSurface = rightsurface
+				p.TopSurface = topsurface
+				p.Transparency = transparency
+				p.Material = material
+				if p.Transparency>0.285 then -- make glass shatter lol
+					p.Anchored = false
+				else
+					p.Anchored=true
+				end
+				p.CFrame = cframe
+				p.Reflectance = reflectance
+			end
+			return -- stop the function
+		end
+		local mody = math.random(-125,125)/1000 -- some randomization
+		for y = 1,yi do
+			if math.random()> 0.5 then
+				local modx = math.random(-125,125)/1000
+				for x = 1,xi do
+					local modz = math.random(-125,125)/1000
+					for z = 1,zi do -- offset = x/xi-0.75+modx)
+						fragmentate(cframe*CFrame.new(size.X*(xi==1 and 0 or x/xi-0.75+modx),size.Y*(yi==1 and 0 or y/yi-0.75+mody),size.Z*(zi==1 and 0 or z/zi-0.75+modz)), -- maths
+							Vector3.new(xi == 2 and size.X*(1-2*math.abs(x/xi-0.75+modx)) or size.X,yi == 2 and size.Y*(1-2*math.abs(y/yi-0.75+mody)) or size.Y,
+								zi == 2 and size.Z*(1-2*math.abs(z/zi-0.75+modz)) or size.Z or nil),color,explosion_position,explosion_blastradius,
+							z~=zi and surface_between_splitted_parts or backsurface,y==2 and surface_between_splitted_parts or bottomsurface,
+							z==2 and surface_between_splitted_parts or frontsurface,x==2 and surface_between_splitted_parts or leftsurface,x~=xi and surface_between_splitted_parts or rightsurface,
+							y~=yi and surface_between_splitted_parts or topsurface,transparency,reflectance,material) 
+					end
+
+				end
+			else
+				local modz = math.random(-125,125)/1000
+				for z = 1,zi do
+					local modx = math.random(-125,125)/1000
+					for x = 1,xi do
+						fragmentate(cframe*CFrame.new(size.X*(xi==1 and 0 or x/xi-0.75+modx),size.Y*(yi==1 and 0 or y/yi-0.75+mody),size.Z*(zi==1 and 0 or z/zi-0.75+modz)),
+							Vector3.new(xi == 2 and size.X*(1-2*math.abs(x/xi-0.75+modx)) or size.X,yi == 2 and size.Y*(1-2*math.abs(y/yi-0.75+mody)) or size.Y,
+								zi == 2 and size.Z*(1-2*math.abs(z/zi-0.75+modz)) or size.Z),color,explosion_position,explosion_blastradius,
+							z~=zi and surface_between_splitted_parts or backsurface,y==2 and surface_between_splitted_parts or bottomsurface,
+							z==2 and surface_between_splitted_parts or frontsurface,x==2 and surface_between_splitted_parts or leftsurface,x~=xi and surface_between_splitted_parts or rightsurface,
+							y~=yi and surface_between_splitted_parts or topsurface,transparency,reflectance,material)
+					end
+				end
+			end
+		end				
+	end
+
+	function start_fragmentation(position,radius)
+		local search = Region3.new(position-Vector3.new(radius,radius,radius)*1.1,position+Vector3.new(radius,radius,radius)*1.1)
+		repeat
+			local finish = false
+			local parts = workspace:FindPartsInRegion3WithIgnoreList(search,list,100) -- maximum number of parts that FindPartsInRegion3 can find is 100, so we have to do this to find them all
+			for i = 1,#parts do
+				table.insert(list,1,parts[i])
+			end
+			finish = true
+		until #parts < 100 and finish
+		print(#list)
+		local t = tick()
+		for i = 1,#list do
+			local p = list[i]
+			if p:IsDescendantOf(fragmentable) and p:IsDescendantOf(ch)==false then
+				fragmentate(p.CFrame,p.Size,p.BrickColor,position,radius,p.BackSurface,p.BottomSurface,p.FrontSurface,p.LeftSurface,p.RightSurface,p.TopSurface,p.Transparency,p.Reflectance,p.Material)
+				if #storage < maximumstorage and p.Shape == "Block" then -- recycle them
+					p.Anchored = false
+					p.FormFactor = "Custom"
+					p.Size = stored_partsize
+					p.Position = storage_position
+					table.insert(storage,1,p)
+				else -- storage is full
+					p:Destroy()
+				end
+			end
+		end	
+		list = {}
+	end
+
+	function AngleOfReach(distance, altitude, velocity)
+		local hasGravity, gravity = pcall(function() return workspace.Gravity end) -- support gravity for revs with gravity (2016M and onwards)
+		if not hasGravity then gravity = 196.2 end -- else resort to default
+		local theta = math.atan((velocity^2 + math.sqrt(velocity^4 -gravity*(gravity*distance^2 + 2*altitude*velocity^2)))/(gravity*distance))
+		if theta ~= theta then
+			theta = math.pi/4
+		end
+		return(theta)
+	end
+
+	local mouse;
+
+	local grenade = Instance.new("Tool", game:GetService("Players").LocalPlayer.Backpack)
+	grenade.Name = "Grenade"
+
+	local handle = Instance.new("Part", grenade)
+	handle.Name = "Handle"
+	handle.Size = Vector3.new(0.8, 0.92, 0.8)
+	handle.BrickColor = BrickColor.new(141)
+
+	local mesh = Instance.new("SpecialMesh", handle)
+	mesh.Scale = Vector3.new(1.3, 1.5, 1.3)
+	mesh.MeshId = "rbxassetid://166887416"
+
+	grenade.Equipped:Connect(function(m)
+		mouse = m
+	end)
+
+	grenade.Activated:Connect(function()
+		local position = mouse.Hit.p
+		local handlePos = Vector3.new(handle.Position.X, 0, handle.Position.Z) -- remove Y from the equation, it's not needed
+		local mousePos = Vector3.new(position.X, 0, position.Z) -- ditto
+		local distance = (handlePos - mousePos).magnitude -- distance between mouse and handle
+		local altitude = position.Y - handle.Position.Y -- get altitude
+		local angle = AngleOfReach(distance, altitude, 100) -- calculate the angle
+
+		local newGrenade = handle:Clone()
+		newGrenade.Name = "Grenade"
+		newGrenade.Parent = workspace
+		newGrenade.CanCollide = true
+
+		local position = mouse.Hit.p
+		newGrenade.Velocity = (CFrame.new(newGrenade.Position, Vector3.new(position.X, newGrenade.Position.Y, position.Z)) * CFrame.Angles(angle, 0, 0)).lookVector * 100 -- throwing calc, maybe a tad long lol
+
+		wait(fuse) -- fuse	
+
+		local boom = Instance.new("Explosion")
+		boom.BlastPressure = 0 -- non destructive, since the fragmentation is the destruction
+		boom.BlastRadius = 7.5
+		boom.Hit:Connect(function(hit)
+			if hit.Parent:FindFirstChild("Humanoid") then
+				hit.Parent:breakJoints()
+			end
+		end)
+		boom.Position = newGrenade.Position
+		boom.Parent = workspace
+		start_fragmentation(newGrenade.Position, 7.5)
+		newGrenade:Destroy()
+	end)
+	-- Check Cashed (CheckkCashedd) | 11/15/2024
+end)
+end)()
+
+-- Script @ StarterGui.ScreenGui.ImageLabel.ScrollingFrame.doge.LocalScript
+coroutine.wrap(function()
+local script = LocalScript4
+script.Parent.MouseButton1Click:Connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/ZwDaNk/FDArchives/refs/heads/main/ORC/Backports/doge.lua",true))()
+end)
+
+end)()
+
+-- Script @ StarterGui.ScreenGui.ImageLabel.ScrollingFrame.harktistic.LocalScript
+coroutine.wrap(function()
+local script = LocalScript5
+script.Parent.MouseButton1Click:Connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/ayyyyyy-lmao/FEHaxx/refs/heads/main/CDN/Scripts/STK", true))()
+end)
+
+end)()
+
+-- Script @ StarterGui.ScreenGui.ImageLabel.ScrollingFrame.harkkek4.LocalScript
+coroutine.wrap(function()
+local script = LocalScript6
+script.Parent.MouseButton1Click:Connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/RuMinGay/t0pk3k4.0/refs/heads/main/t0pk3k",true))()
+end)
+end)()
+
+-- Script @ StarterGui.ScreenGui.ImageLabel.ScrollingFrame.harkknifev4.LocalScript
+coroutine.wrap(function()
+local script = LocalScript7
+script.Parent.MouseButton1Click:Connect(function()
+	loadstring(game:HttpGet("https://cdn.jsdelivr.net/gh/eazy-man/febypass@refs/heads/main/harkknifev4febypasstuffohiosigmagrimaceshake.txt.cs.py.lua", true))()
+end)
+
+end)()
+
+-- Script @ StarterGui.ScreenGui.ImageLabel.LocalScript
+coroutine.wrap(function()
+local script = LocalScript8
+script.Parent.Draggable = true
+script.Parent.Active = true
+end)()
+
